f6b911f4c4fddf7e12b6955f5a1091e1d07704e7
Alex Bryan <tabryan1990@gmail.com>
2024-02-28 00:39:09 -0500

    problem 8 solved

--- src/recursion.js
+++ src/recursion.js
@@ -102,6 +102,15 @@ let exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 let powerOfTwo = function(n) {
+    if ( n === 0 ) return false;
+
+    if ( n === 1 ) return true;
+
+    if ( n % 2 !== 0 && n !== 1 ) {
+        return false;
+    } else {
+        return powerOfTwo( n / 2 );
+    }
 };
 
 // 9. Write a function that reverses a string.
--- src/test.js
+++ src/test.js
@@ -1,17 +1,38 @@
-// 7. Compute the exponent of a number.
-// The exponent of a number says how many times the base number is used as a factor.
-// 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
-// exponent(4,3); // 64
-debugger
-let exponent = function(base, exp) {
-    if ( exp === 0 ) return 1;
+// 8. Determine if a number is a power of two.
+// powerOfTwo(1); // true
+// powerOfTwo(16); // true
+// powerOfTwo(10); // false
+let powerOfTwo = function(n) {
+    if ( n === 0 ) return false;
 
-    if ( exp < 0 ) {
-        return 1/(base * exponent(base, -exp - 1));
-    } else if ( exp > 0 ) {
-        return base * exponent(base, exp - 1);
+    if ( n === 1 ) return true;
+
+    if ( n % 2 !== 0 && n !== 1 ) {
+        return false;
+    } else {
+        return powerOfTwo( n / 2 );
     }
 };
 
-// console.log(exponent(4,3)); // 64
-console.log(exponent(4,-2)); // 0.0625
+console.log(powerOfTwo(1)); // true
+console.log(powerOfTwo(16)); // true
+console.log(powerOfTwo(10)); // false
+console.log(powerOfTwo(0)); // false
+console.log(powerOfTwo(270)); // false
+
+
+/*
+
+let isEven = function(n) {
+    n = Math.abs(n);
+
+    if (n == 0) {
+        return true;
+    } else if (n == 1) {
+        return false;
+    } else {
+        return isEven(n - 2);
+    }
+};
+
+ */
